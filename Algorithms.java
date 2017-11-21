afb9f1b0dd2c2afb50e952544cbc9b0b7f576155
costnerjc <costnerjc@email.appstate.edu>
2017-11-20 19:32:10 -0500

    Wrote the test in main
    Wrote a main in Algorithms.java and separated algorithms out into their own classes

--- Algorithms.java
+++ Algorithms.java
@@ -1,130 +1,72 @@
-import java.util.*; 
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Algorithms
 {
-    
-}
+    public static void main(String[] args) {
+      // create and initialize the wikipedia sized array
+      int[] wiki = new int[5505945];
+      for (int i = 0; i < wiki.length; i++)
+        wiki[i] = i;
 
-class ShellSort
-{
-    /* An utility function to print array of size n*/
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
+      // randomize the array
+      shuffleArray(wiki);
+
+      // make 4 copies, since arrays are stored globally
+      int[] wiki1 = new int[5505945];
+      int[] wiki2 = new int[5505945];
+      int[] wiki3 = new int[5505945];
+      int[] wiki4 = new int[5505945];
+
+      System.arraycopy( wiki, 0, wiki1, 0, wiki.length );
+      System.arraycopy( wiki, 0, wiki2, 0, wiki.length );
+      System.arraycopy( wiki, 0, wiki3, 0, wiki.length );
+      System.arraycopy( wiki, 0, wiki4, 0, wiki.length );
+
+      // Create algorithm objects...
+      ShellSort shellSort = new ShellSort();
+      HeapSort heapSort = new HeapSort();
+      MergeSort mergeSort = new MergeSort();
+      QuickSort quickSort = new QuickSort();
+
+      // Get starting time, run algorithm, print time took, repeat...
+
+      // For Shellsort
+      double startTime = System.nanoTime();
+      shellSort.sort(wiki1);
+      System.out.println("Shellsort took: " + ((System.nanoTime() - startTime) / Math.pow(10, 9)) +
+                          " Seconds");
+      // For Heapsort
+      startTime = System.nanoTime();
+      heapSort.sort(wiki2);
+      System.out.println("Heapsort took: " + ((System.nanoTime() - startTime) / Math.pow(10, 9)) +
+                          " Seconds");
+      // For Mergesort
+      startTime = System.nanoTime();
+      mergeSort.sort(wiki3, 0, wiki3.length-1);
+      System.out.println("Mergesort took: " + ((System.nanoTime() - startTime) / Math.pow(10, 9)) +
+                          " Seconds");
+      // For Quicksort
+      startTime = System.nanoTime();
+      quickSort.sort(wiki4, 0, wiki4.length-1);
+      System.out.println("Quicksort took: " + ((System.nanoTime() - startTime) / Math.pow(10, 9)) +
+                          " Seconds");
     }
-    double getTime(int arr[])
+
+    //Fisher-Yates shuffling algorighm
+    static void shuffleArray(int[] ar)
     {
-        ShellSort ob = new ShellSort();
-        double startTime = System.nanoTime();
-        ob.sort(arr);
-        return (System.nanoTime() - startTime);
+      Random rnd = ThreadLocalRandom.current();
+      for (int i = ar.length - 1; i > 0; i--)
+      {
+        int index = rnd.nextInt(i + 1);
+        // Simple swap
+        int a = ar[index];
+        ar[index] = ar[i];
+        ar[i] = a;
+      }
     }
- 
-    /* function to sort arr using shellSort */
-    int sort(int arr[])
-    {
-        int n = arr.length;
- 
-        // Start with a big gap, then reduce the gap
-        for (int gap = n/2; gap > 0; gap /= 2)
-        {
-            // Do a gapped insertion sort for this gap size.
-            // The first gap elements a[0..gap-1] are already
-            // in gapped order keep adding one more element
-            // until the entire array is gap sorted
-            for (int i = gap; i < n; i += 1)
-            {
-                // add a[i] to the elements that have been gap
-                // sorted save a[i] in temp and make a hole at
-                // position i
-                int temp = arr[i];
- 
-                // shift earlier gap-sorted elements up until
-                // the correct location for a[i] is found
-                int j;
-                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
-                    arr[j] = arr[j - gap];
- 
-                // put temp (the original a[i]) in its correct
-                // location
-                arr[j] = temp;
-            }
-        }
-        return 0;
-    }
- 
-    // Driver method
-    public static void main(String args[])
-    {
-        int arr[] = {12, 34, 54, 2, 3};
-        System.out.println("Array before sorting");
-        printArray(arr);
- 
-        ShellSort ob = new ShellSort();
-        ob.sort(arr);
- 
-        System.out.println("Array after sorting");
-        printArray(arr);
-    }
-} 
-/* Heap sort is a comparison based sorting technique based on Binary Heap 
-data structure. It is similar to selection sort where we first find the maximum 
-element and place the maximum element at the end. 
-We repeat the same process for remaining element. */
-class HeapSort
-{
-    public void sort(int arr[])
-    {
-        int n = arr.length;
- 
-        // Build heap (rearrange array)
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
- 
-        // One by one extract an element from heap
-        for (int i=n-1; i>=0; i--)
-        {
-            // Move current root to end
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
- 
-            // call max heapify on the reduced heap
-            heapify(arr, i, 0);
-        }
-    }
- 
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    void heapify(int arr[], int n, int i)
-    {
-        int largest = i;  // Initialize largest as root
-        int l = 2*i + 1;  // left = 2*i + 1
-        int r = 2*i + 2;  // right = 2*i + 2
- 
-        // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
- 
-        // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
- 
-        // If largest is not root
-        if (largest != i)
-        {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
- 
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
-        }
-    }
- 
+
     /* A utility function to print array of size n */
     static void printArray(int arr[])
     {
@@ -133,203 +75,4 @@ class HeapSort
             System.out.print(arr[i]+" ");
         System.out.println();
     }
-    double getTime(int arr[])
-    {
-        HeapSort ob = new HeapSort();
-        double startTime = System.nanoTime();
-        ob.sort(arr);
-        return (System.nanoTime() - startTime);
-    }
- 
-    // Driver program
-    public static void main(String args[])
-    {
-        int arr[] = {12, 11, 13, 5, 6, 7};
-        int n = arr.length;
- 
-        HeapSort ob = new HeapSort();
-        ob.sort(arr);
- 
-        System.out.println("Sorted array is");
-        printArray(arr);
-    }
 }
-
-
-class MergeSort
-{
-    // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
-    void merge(int arr[], int l, int  m, int r)
-    {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
- 
-        /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
- 
-        /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
- 
- 
-        /* Merge the temp arrays */
- 
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
- 
-        // Initial index of merged subarry array
-        int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
- 
-        /* Copy remaining elements of L[] if any */
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
- 
-        /* Copy remaining elements of R[] if any */
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
- 
-    // Main function that sorts arr[l..r] using
-    // merge()
-    void sort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
-            // Find the middle point
-            int m = (l+r)/2;
- 
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr , m+1, r);
- 
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
-    }
- 
-    /* A utility function to print array of size n */
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-    double getTime(int arr[])
-    {
-        MergeSort ob = new MergeSort();
-        int length = arr.length - 1;
-        double startTime = System.nanoTime();
-        ob.sort(arr, 0, length);
-        return (System.nanoTime() - startTime);
-    }
-    // Driver method
-    public static void main(String args[])
-    {
-        int arr[] = {12, 11, 13, 5, 6, 7};
- 
-        System.out.println("Given Array");
-        printArray(arr);
- 
-        MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.length-1);
- 
-        System.out.println("\nSorted array");
-        printArray(arr);
-    }
-}
-
-
-class QuickSort
-{
-     /*Code retrieved from
-     http://www.geeksforgeeks.org/quick-sort/
-     this func take last element as pivot,   
-     places the pivot element at its correct
-     position in sorted array, and places all
-     smaller (smaller than pivot) to left of
-     pivot and all greater elements to right
-     of pivot */
-    public int partition(int arr[], int low, int high)
-    {
-        int pivot = arr[high]; 
-        int i = (low-1); // index of smaller element
-        for (int j=low; j<high; j++)
-        {
-            // If current element is smaller than or
-            // equal to pivot
-            if (arr[j] <= pivot)
-            {
-                i++;
- 
-                // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
- 
-         // swap arr[i+1] and arr[high] (or pivot)
-         int temp = arr[i+1];
-         arr[i+1] = arr[high];
-         arr[high] = temp;
- 
-         return i+1;
-    }
- 
-    /* The main function that implements QuickSort()
-    arr[] --> Array to be sorted,
-    low  --> Starting index,
-    high  --> Ending index */
-    void sort(int arr[], int low, int high)
-    {
-        if (low < high)
-        {
-            /* pi is partitioning index, arr[pi] is 
-            now at right place */
-            int pi = partition(arr, low, high);
- 
-            // Recursively sort elements before
-            // partition and after partition
-            sort(arr, low, pi-1);
-            sort(arr, pi+1, high);
-         }
-    }
-    double getTime(int arr[])
-    {
-        MergeSort ob = new MergeSort();
-        int length = arr.length - 1;
-        double startTime = System.nanoTime();
-        ob.sort(arr, 0, length);
-        return (System.nanoTime() - startTime);
-    }
-}
-
